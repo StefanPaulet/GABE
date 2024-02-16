d7741c04602731b87ae56237bc08a3a94287851e
stefan <tudor.stefan.paulet@gmail.com>
2024-02-16 13:50:06 +0200

    Added test for NeuralNetwork

--- test/unittest/NeuralNetTest.cpp
+++ test/unittest/NeuralNetTest.cpp
@@ -8,17 +8,23 @@
 namespace {
 using namespace gabe::utils::math;
 using namespace gabe::nn;
+using namespace layer;
+using linearArray::larray;
 } // namespace
 
-//TEST(NeuralNetwork, Construction) {
-//  NeuralNetwork<float, 8, HiddenLayer<sigmoidFunction, 5>, HiddenLayer<reluFunction, 3>,
-//                OutputLayer<sigmoidFunction, meanSquaredError, 3>>
-//      nn {};
-//}
-//
-//TEST(NeuralNetwork, ForwardPropagating) {
-//  NeuralNetwork<float, 8, HiddenLayer<sigmoidFunction, 5>, HiddenLayer<reluFunction, 3>,
-//                OutputLayer<sigmoidFunction, meanSquaredError, 3>>
-//      nn {};
-//  std::cout << nn.feedForward(linearArray::larray(1.0f, 2, 3, 4, 5, 6, 7, 8)) << '\n';
-//}
+TEST(NeuralNetwork, Construction) {
+  NeuralNetwork<float, InputLayer<float, 3>, Layer<float, IdentityFunction<>, 3>> nn;
+  static_assert(std::is_same_v<decltype(nn.weights(0)), SquareLinearMatrix<float, 3>&>);
+  static_assert(std::is_same_v<decltype(nn.biases(0)), LinearColumnArray<float, 3>&>);
+  (void) nn;
+}
+
+TEST(NeuralNetwork, ForwardPropagating) {
+  NeuralNetwork<float, InputLayer<float, 3>, Layer<float, IdentityFunction<>, 3>> nn;
+  auto input = larray(larray(1.0f, 2, 3));
+  nn.weights(0) = larray(larray(1.0f, 1, 1), larray(1.0f, 1, 1), larray(1.0f, 1, 1));
+  nn.biases(0) = larray(larray(1.0f, 2, 3)).transpose();
+  auto rez = nn.feedForward(input.transpose());
+  auto expected = larray(larray(7.0f, 8, 9)).transpose();
+  ASSERT_EQ(rez, expected);
+}
