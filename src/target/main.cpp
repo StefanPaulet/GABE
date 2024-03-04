8534b4d02802a352be3035bc00a44ebfb866cc47
stefan <tudor.stefan.paulet@gmail.com>
2024-03-04 16:38:37 +0200

    Dataloader test

--- src/target/main.cpp
+++ src/target/main.cpp
@@ -9,11 +9,8 @@ namespace {
 using namespace gabe::utils::math;
 using namespace gabe::utils::dataLoader;
 } // namespace
-#include <cstdint>
 
 
 int main() {
   auto v = loadMNIST<LinearArray<float, 28 * 28>>("/mnt/SSD-SATA/Clion/GABE/datasets/mnist/test", MNISTDataSet::TEST);
-  std::cout << v[0].data << '\n';
-  std::cout << v[0].label << '\n';
 }
---
+++ test/unittest/DataLoaderTest.cpp
@@ -0,0 +1,38 @@
+//
+// Created by stefan on 3/4/24.
+//
+
+#include "gtest/gtest.h"
+#include <utils/dataLoader/DataLoader.hpp>
+#include <utils/math/linearArray/LinearArray.hpp>
+namespace {
+using namespace gabe::utils::math;
+using namespace gabe::utils::dataLoader;
+} // namespace
+
+
+TEST(DataLoader, LoadMNIST) {
+
+  auto array_test =
+      loadMNIST<LinearArray<float, 28 * 28>>("/mnt/SSD-SATA/Clion/GABE/datasets/mnist/test", MNISTDataSet::TEST);
+  auto mtrx_test =
+      loadMNIST<LinearArray<float, 28, 28>>("/mnt/SSD-SATA/Clion/GABE/datasets/mnist/test", MNISTDataSet::TEST);
+  ASSERT_EQ(array_test.size(), 10000);
+  ASSERT_EQ(mtrx_test.size(), 10000);
+  static_assert(std::is_same_v<decltype(array_test[0].data), LinearArray<float, 28 * 28>>);
+  static_assert(std::is_same_v<decltype(array_test[0].label), float>);
+  static_assert(std::is_same_v<decltype(mtrx_test[0].data), LinearArray<float, 28, 28>>);
+  static_assert(std::is_same_v<decltype(mtrx_test[0].label), float>);
+
+
+  auto array_train =
+      loadMNIST<LinearArray<float, 28 * 28>>("/mnt/SSD-SATA/Clion/GABE/datasets/mnist/train", MNISTDataSet::TRAIN);
+  auto mtrx_train =
+      loadMNIST<LinearArray<float, 28, 28>>("/mnt/SSD-SATA/Clion/GABE/datasets/mnist/train", MNISTDataSet::TRAIN);
+  ASSERT_EQ(array_train.size(), 60000);
+  ASSERT_EQ(mtrx_train.size(), 60000);
+  static_assert(std::is_same_v<decltype(array_train[0].data), LinearArray<float, 28 * 28>>);
+  static_assert(std::is_same_v<decltype(array_train[0].label), float>);
+  static_assert(std::is_same_v<decltype(mtrx_train[0].data), LinearArray<float, 28, 28>>);
+  static_assert(std::is_same_v<decltype(mtrx_train[0].label), float>);
+}
